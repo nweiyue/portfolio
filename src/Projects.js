d33fa84f8b9512fa2bcc77e0a044e7df64f5c479
nweiyue <weiyue16@gmail.com>
2021-06-08 00:02:13 +0800

    Add details to project

--- src/Projects.js
+++ src/Projects.js
@@ -3,88 +3,243 @@ import React from "react";
 import Template from "./Template";
 import Content from "./Content";
 
-import Carousel from "react-bootstrap/Carousel";
-import Media from "react-bootstrap/Media";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 import imgLastMinute from "./resources/pictures/lastMinute_thumbnail.png";
+import imgChampion from "./resources/pictures/champion_thumbnail.png";
+import imgSnake from "./resources/pictures/snake_thumbnail.png";
+import imgRemindMe from "./resources/pictures/remindMe_thumbnail.png";
+import imgAtas from "./resources/pictures/atas_thumbnail.png";
+import imgDuke from "./resources/pictures/duke_thumbnail.png";
+import imgPortfolioWebsiteV1 from "./resources/pictures/portfolioWebsiteV1_thumbnail.png";
 
 export default function Projects(props) {
   return (
     <div>
       <Template title={props.title} subTitle={props.subTitle} />
       <Content>
+        {/* First row of projects */}
         <CardDeck>
+          {/* Portfolio website v1 */}
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={imgLastMinute} />
+            <Card.Img variant="top" src={imgPortfolioWebsiteV1} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Personal Portfolio Website v1.0</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                The first version of my personal portfolio website which was
+                used to showcase my past projects. The current website you are
+                on is the new and improved v2.0!
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <ButtonGroup>
+                <Button
+                  clasName="github-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(
+                      "https://github.com/nweiyue/Portfolio-Website",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <i className="fab fa-github"></i> Github
+                </Button>
+                <Button
+                  clasName="web-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(
+                      "https://nweiyue.github.io/Portfolio-Website/",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <i className="fas fa-desktop"></i> Website
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
+          {/* ATAS */}
           <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={imgLastMinute} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={imgLastMinute} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+            <Card.Img variant="top" src={imgAtas} />
+            <Card.Body>
+              <Card.Title>Addendum for Teaching Assistants (ATAS)</Card.Title>
+              <Card.Text>
+                ATAS is a desktop application that aims to elevate user's
+                efficiency and make their experience of handling administrative
+                tasks in classes as seamless as possible.
+              </Card.Text>
+              <Button
+                className="github-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open(
+                    "https://github.com/AY2021S1-CS2103T-W16-4/tp",
+                    "_blank"
+                  );
+                }}
+              >
+                <i className="fab fa-github"></i> GitHub
+              </Button>
+            </Card.Body>
+          </Card>
+          {/* Duke*/}
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={imgDuke} />
+            <Card.Body>
+              <Card.Title>Duke</Card.Title>
+              <Card.Text>
+                Duke is a is a desktop application which allows users to manage
+                their tasks and at the same time acts as a personal chat bot.
+              </Card.Text>
+              <Button
+                className="github-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open("https://github.com/nweiyue/ip", "_blank");
+                }}
+              >
+                <i className="fab fa-github"></i> GitHub
+              </Button>
+            </Card.Body>
+          </Card>
         </CardDeck>
+        {/* Second row of projects */}
         <CardDeck>
           <Card style={{ width: "18rem" }}>
             <Card.Img variant="top" src={imgLastMinute} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>lastMinute</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                lastMinute is an all-in-one android travel application that
+                redefines users' travel experiences by eliminating travel
+                inconveniences.
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <ButtonGroup>
+                <Button
+                  clasName="github-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(
+                      "https://github.com/LeeMingDe/Orbital2020",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <i className="fab fa-github"></i> Github
+                </Button>
+                <Button
+                  clasName="youtube-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(
+                      "https://www.youtube.com/watch?v=Jg2CCN1vddk&feature=youtu.be",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <i className="fab fa-youtube"></i> Demo
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
           <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={imgLastMinute} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={imgLastMinute} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+            <Card.Img variant="top" src={imgSnake} />
+            <Card.Body>
+              <Card.Title>Snake</Card.Title>
+              <Card.Text>
+                A copy of the classic snake game. Feel free to try it out!
+              </Card.Text>
+              <ButtonGroup>
+                <Button
+                  clasName="github-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://github.com/nweiyue/Snake", "_blank");
+                  }}
+                >
+                  <i className="fab fa-github"></i> Github
+                </Button>
+                <Button
+                  clasName="game-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://nweiyue.github.io/Snake/", "_blank");
+                  }}
+                >
+                  <i className="fas fa-gamepad"></i> Play
+                </Button>
+              </ButtonGroup>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={imgChampion} />
+            <Card.Body>
+              <Card.Title>
+                Offense win games, defense win championships
+              </Card.Title>
+              <Card.Text>
+                A data science project that aims to uncover the truth behind
+                this commonly spoken phrase in the sports world, specifically in
+                the NBA.
+              </Card.Text>
+              <ButtonGroup>
+                <Button
+                  clasName="github-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(
+                      "https://github.com/nweiyue/defense_wins_championships_data_analysis",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <i className="fab fa-github"></i> Github
+                </Button>
+                <Button
+                  clasName="medium-btn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.open(
+                      "https://medium.com/@nweiyue/nba-offense-win-games-defense-win-championships-is-this-really-the-case-6faefe6d9c9b?source=friends_link&sk=a8356f7c4d3902ee01272cea463e8e88",
+                      "_blank"
+                    );
+                  }}
+                >
+                  <i className="fab fa-medium"></i> Read
+                </Button>
+              </ButtonGroup>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+        {/* Third row of projects */}
+        <CardDeck>
+          <Card style={{ width: "21.3rem" }}>
+            <Card.Img variant="top" src={imgRemindMe} />
+            <Card.Body>
+              <Card.Title>RemindMe Bot</Card.Title>
+              <Card.Text>
+                RemindMe Bot is a simple python telegram bot that allows users to
+                set reminders base on a timer.
+              </Card.Text>
+              <Button
+                clasName="github-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open(
+                    "https://github.com/nweiyue/remindMeBot",
+                    "_blank"
+                  );
+                }}
+              >
+                <i className="fab fa-github"></i> Github
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card></Card>
+          <Card></Card>
         </CardDeck>
       </Content>
     </div>
